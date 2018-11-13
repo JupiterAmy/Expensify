17aaccd26a31a8896460e34e9a4101bbc8dcd6c4
JupiterAmy <amritansh.200792@gmail.com>
2018-11-13 14:43:05 +0530

    modiefied app.js

--- src/app.js
+++ src/app.js
@@ -9,18 +9,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-
-
 const store = configureStore();
-
-const expenseOne = store.dispatch(addExpense({ description: 'Water Bill', amount: 4500, createdAt: 100 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
-const expenseThree = store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
     <Provider store={store}>
